d19282b88e6f86b63d86a17e10337830f9cc5b40
slembcke <slembcke@gmail.com>
2014-10-26 18:35:14 -0500

    Initial commit

---
+++ Source/MainMenu.m
@@ -0,0 +1,5 @@
+#import "MainMenu.h"
+
+@implementation MainMenu
+
+@end
---
+++ Source/Platforms/Android/java/org/cocos2d/GalacticGuardian/GalacticGuardianActivity.java
@@ -0,0 +1,7 @@
+package org.cocos2d.GalacticGuardian;
+
+import org.cocos2d.CCActivity;
+
+public class GalacticGuardianActivity extends CCActivity {
+
+}
